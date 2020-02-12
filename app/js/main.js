d5b90c62e8a81112e67677d05c8727f5e654450c
projectVD <vladdegtarov1998@gmail.com>
2020-02-12 16:00:44 +0200

    fixed sliders, start adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ $(function () {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
 
     $('.slider-dotshead').slick({
